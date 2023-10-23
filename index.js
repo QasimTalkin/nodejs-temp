89ff3bdf7de7aa347e8ee87f9428e3525e6468d6
Abul Qasim <qasim@abuls-mbp.tail79812.ts.net>
2023-10-23 15:30:29 -0400

    base template

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const http = require('http');
+const express = require("express")
 const PORT = 3000;
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
-});
+app.get('/', (req, res)=>{
+  res.json([{
+    id:1,
+    name:"qazsim"
+  },
+{
+  id:2,
+  name:"game"
+}
+])
+})
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+app.listen(PORT, ()=>console.log(`Server running at http://localhost:${PORT}/`))
+
+// const http = require('http');
+
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World!');
+// });
+
+// server.listen(PORT, () => {
+//   console.log(`Server running at http://localhost:${PORT}/`);
+// });
